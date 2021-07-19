d62bfbc734e7172991134172539b6d89882dbf05
kanbaru-github <usupfv@gmail.com>
2021-07-19 07:06:10 +0000

    radio

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,10 @@ class Product < ApplicationRecord
   belongs_to :genre
   attachment :image
 
+  def add_tax_price
+    (self.price * 1.1).round
+  end
+
+  enum is_active: { on_sale: 0, draft: 1 }
+
 end
