14c8d49a54d62e5f37f90f10c73ce5db346a0db5
nao0725 <aqua.725.times@gmail.com>
2021-07-19 01:22:34 +0000

    Create admin customer show.html.erb && kaminari

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -3,10 +3,12 @@ class Admin::CustomersController < ApplicationController
   # before_action :authenticate_admin
 
   def index
-    @customers = Customer.all
+    # @customers = Customer.all kaminari使用の為
+    @customers = Customer.page(params[:page]).reverse_order
   end
 
   def show
+    @customer = Customer.find(params[:id])
   end
 
   def edit
