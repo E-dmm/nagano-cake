2f2f6c35b9e919c586646679a4b3429d216c8de4
nao0725 <aqua.725.times@gmail.com>
2021-07-21 07:06:57 +0000

    Fix Delivery_address index&&edit

--- app/controllers/public/delivery_addresses_controller.rb
+++ app/controllers/public/delivery_addresses_controller.rb
@@ -1,14 +1,16 @@
 class Public::DeliveryAddressesController < ApplicationController
 
   def index
+    @delivery_address = DeliveryAddress.new
     @delivery_addresses = DeliveryAddress.all
   end
 
 
   def create
     @delivery_address = DeliveryAddress.new(delivery_address_params)
+    @delivery_address.customer_id = current_customer.id
     @delivery_address.save
-    redirect_to public_delivery_addresses_path
+    redirect_to delivery_addresses_path
   end
 
   def edit
@@ -17,20 +19,21 @@ class Public::DeliveryAddressesController < ApplicationController
 
   def update
     @delivery_address = DeliveryAddress.find(params[:id])
+    @delivery_address.customer_id = current_customer.id
     @delivery_address.update(delivery_address_params)
-    redirect_to public_delivery_addresses_path
+    redirect_to delivery_addresses_path
   end
 
   def destroy
     @delivery_address = DeliveryAddress.find(params[:id])
     @delivery_address.destroy
-    redirect_to public_delivery_addresses_path
+    redirect_back(fallback_location: root_path)
   end
 
   private
 
   def delivery_address_params
-    params.permit(:postcode, :address, :name)
+    params.require(:delivery_address).permit(:postcode, :address, :name)
     #エラーが起きたためrequire消しました
   end
 end
