0a627361c4d4ca74568df10162ee343143a336f2
shotaro0619 <66shhotaro99@gmail.com>
2021-07-22 03:04:38 +0000

    admin-login

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,65 +6,65 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# Admin.create!(
-#    email: 'test@test.com',
-#    password: 'testtest')
+Admin.create!(
+    email: 'test@test.com',
+    password: 'testtest')
 
 # Customer.create!(
-#    email: 'test@test.com',
-#    password: 'testtest',
-#    last_name: 'test',
-#    first_name: 'test',
-#    last_name_kana: 'test',
-#    first_name_kana: 'test',
-#    postcode: '0000000',
-#    address: 'test',
-#    phone_number: '00000000000',
-#    is_delete: 'false'
-#    )
+#     email: 'test@test.com',
+#     password: 'testtest',
+#     last_name: 'test',
+#     first_name: 'test',
+#     last_name_kana: 'test',
+#     first_name_kana: 'test',
+#     postcode: '0000000',
+#     address: 'test',
+#     phone_number: '00000000000',
+#     is_delete: 'false'
+#     )
 
 # Product.create!(
-#    genre_id: 1,
-#    name: 'test',
-#    text: 'test',
-#    price: 100,
-#    is_active: '販売中',
-#    )
+#     genre_id: 1,
+#     name: 'test',
+#     text: 'test',
+#     price: 100,
+#     is_active: '販売中',
+#     )
 
 # CartProduct.create!(
-#    product_id: 1,
-#    customer_id: 1,
-#    quantity: 1
-#    )
+#     product_id: 1,
+#     customer_id: 1,
+#     quantity: 1
+#     )
 
 # Order.create!(
-#    customer_id: 1,
-#    payment: 1,
-#    postcode: '1111111',
-#    address: 'tokyo',
-#    total_price: 910,
-#    address_name: 'hoge',
-#    shipping: 800,
-#    order_status: 1,
+#     customer_id: 1,
+#     payment: 1,
+#     postcode: '1111111',
+#     address: 'tokyo',
+#     total_price: 910,
+#     address_name: 'hoge',
+#     shipping: 800,
+#     order_status: 1,
 # )
 
 # DeliveryAddress.create!(
-#    customer_id: 1,
-#    postcode: '1111111',
-#    name: 'dmm',
-#    address: 'osaka'
-#    )
+#     customer_id: 1,
+#     postcode: '1111111',
+#     name: 'dmm',
+#     address: 'osaka'
+#     )
 
 # Genre.create!(
-#    name: 'sample',
-#    )
+#     name: 'sample',
+#     )
 
 # OrderedProduct.create!(
-#    product_id: 1,
-#    order_id: 1,
-#    price: 100,
-#    quantity: 1,
-#    production_status: 0,
-#    )
+#     product_id: 1,
+#     order_id: 1,
+#     price: 100,
+#     quantity: 1,
+#     production_status: 0,
+#     )
 
 
