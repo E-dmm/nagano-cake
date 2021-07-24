3aef50c2534229048844c3487dce8176d560b16b
nao0725 <aqua.725.times@gmail.com>
2021-07-24 00:09:06 +0000

    Fix layout to admin/customer/edit.html.erb

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -15,9 +15,9 @@ class Admin::CustomersController < ApplicationController
   end
 
   def update
-    @customer = current_customer
+    @customer = Customer.find(params[:id])
     @customer.update(customer_params)
-    redirect_to admin_customer_path(current_customer)
+    redirect_to admin_customer_path(@customer)
   end
 
   private
