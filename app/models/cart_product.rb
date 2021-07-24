d0e881fafb1c35d6db4953ac732b3e51ba31894f
kanbaru-github <usupfv@gmail.com>
2021-07-24 08:26:04 +0000

    cart_layout

--- app/models/cart_product.rb
+++ app/models/cart_product.rb
@@ -3,6 +3,8 @@ class CartProduct < ApplicationRecord
   belongs_to :product
   belongs_to :customer
 
+  validates :quantity, presence: true
+
   def add_tax_price
     (self.price * 1.1).round
   end
