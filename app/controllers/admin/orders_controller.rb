f6aceea6d302708dffa36c360b4595c8e333ce00
nao0725 <aqua.725.times@gmail.com>
2021-07-25 01:26:00 +0000

    Fix views layout & others

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,6 +1,5 @@
 class Admin::OrdersController < ApplicationController
 
-  # before_action :authenticate_admin
 
   def show
     @customer = Customer.find(params[:id])
--- app/models/ordered_product.rb
+++ app/models/ordered_product.rb
@@ -11,7 +11,7 @@ class OrderedProduct < ApplicationRecord
   end
 
   enum production_status: {
-     制作不可:0,制作待ち:1,製作中:2,製作終了:3
+     製作不可:0,製作待ち:1,製作中:2,製作終了:3
   }
 
 end
